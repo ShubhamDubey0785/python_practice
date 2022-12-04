31f61d5835cb985e4c4e6c78e0d41cb2edda9793
ShubhamDubey0785 <114800339+ShubhamDubey0785@users.noreply.github.com>
2022-12-04 16:14:02 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,16 @@
+# set store multiple data type in a single variable
+# unordered
+# unchangeable/immutable
+# non-homogeneous
+# do not allow duplicate value
+
+
+# mySet={"car","boat","train","car"}
+# print(mySet)
+# if "car" in mySet:
+#     print("car")
+
+# print(HELLO)
+tuple1=("H","E","L","L","O")
+for i in tuple1:
+    print(i,end="")
