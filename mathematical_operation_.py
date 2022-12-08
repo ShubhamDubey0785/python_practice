f5252708c03114a3045837456744abb278a238c8
ShubhamDubey0785 <114800339+ShubhamDubey0785@users.noreply.github.com>
2022-12-09 00:37:47 +0530

    Add files via upload

---
+++ mathematical_operation_.py
@@ -0,0 +1,11 @@
+a=int(input())
+b=int(input())
+def function(x,y):
+    return x%y
+print(function(x=a,y=b))
+def div(x,y):
+    return x//y
+print(div(x=a,y=b))
+def mod(x,y):
+    return x**y
+print(mod(x=a,y=b))
