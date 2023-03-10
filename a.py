fc2934b02a59d16db432a282433748346bd7415a
ShubhamDubey0785 <114800339+ShubhamDubey0785@users.noreply.github.com>
2023-03-10 22:35:17 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,13 @@
+print('Hello world')
+print("Hello world")
+print('"Hello world"')
+print('Day 1 - Python Print Function')
+print("The function is declared like this:")
+print("print('what to print')")
+print("Hello world\nHello world\nHello world")
+print("Hello"+"World")
+print("Hello "+"World")
+print("Hello"+" World")
+print("Hello","World")
+
+print("Hello"+" "+"World")
