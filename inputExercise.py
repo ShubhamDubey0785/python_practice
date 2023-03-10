ede98fbd9d680a3372f9dff8d8bb0896c1cb8e33
ShubhamDubey0785 <114800339+ShubhamDubey0785@users.noreply.github.com>
2023-03-10 22:36:46 +0530

    Add files via upload

---
+++ inputExercise.py
@@ -0,0 +1,8 @@
+x=input("Enter your string here: ")
+print(len(x))
+# second method
+print(len(input("Enter your name here: ")))
+#  Variable
+name=input("What is your name")
+print(name)
+A="Dr Rudrakshmani pratap janardan reddy"
